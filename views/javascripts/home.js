8e39ef1c4a81e2c8b34859c370cdb4415008912d
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2022-01-06 00:50:30 +0530

    updated home page

--- views/javascripts/home.js
+++ views/javascripts/home.js
@@ -1,3 +1,16 @@
 async function bodyLoad() {
     console.log(`Welcome to mapleclub.top`)
 }
+
+async function showMenu() {
+    var showIcon = document.getElementById('showMenu')
+    var hideIcon = document.getElementById('hideMenu')
+    showIcon.style.display = 'none'
+    hideIcon.style.display = 'block'
+}
+async function hideMenu() {
+    var showIcon = document.getElementById('showMenu')
+    var hideIcon = document.getElementById('hideMenu')
+    showIcon.style.display = 'block'
+    hideIcon.style.display = 'none'
+}
